bba31a86b9af1e2af0a43c356beb93fd64cd9a6d
celiapaivab <celiadepaivabruno@gmail.com>
2025-08-13 17:50:52 -0300

    Headless mode included

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context, scenario):
-    # Inicializa o driver antes de cada cenário
-    context.driver = webdriver.Chrome()
-    context.driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # roda sem interface gráfica
+    chrome_options.add_argument("--no-sandbox")  # necessário no Actions
+    chrome_options.add_argument("--disable-dev-shm-usage")  # evita problemas de memória
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-allow-origins=*")
+    chrome_options.add_argument("--user-data-dir=/tmp/temp-chrome-profile")  # diretório único
+
+    context.driver = webdriver.Chrome(options=chrome_options)
 
 def after_scenario(context, scenario):
     # Fecha o navegador após cada cenário
-    context.driver.quit()
+    if hasattr(context, "driver"):
+        context.driver.quit()
