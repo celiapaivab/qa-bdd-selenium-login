7b41c2a647066bf0a549e1f5f47a5e73a407decf
celiapaivab <celiadepaivabruno@gmail.com>
2025-08-13 17:54:41 -0300

    Environment updated

--- features/environment.py
+++ features/environment.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def before_scenario(context, scenario):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # roda sem interface gráfica
-    chrome_options.add_argument("--no-sandbox")  # necessário no Actions
-    chrome_options.add_argument("--disable-dev-shm-usage")  # evita problemas de memória
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--remote-allow-origins=*")
-    chrome_options.add_argument("--user-data-dir=/tmp/temp-chrome-profile")  # diretório único
+def create_chrome_driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-allow-origins=*")
+    options.add_argument("--user-data-dir=/tmp/temp-chrome-profile")
+    return webdriver.Chrome(options=options)
 
-    context.driver = webdriver.Chrome(options=chrome_options)
+def before_scenario(context, scenario):
+    context.driver = create_chrome_driver()
 
 def after_scenario(context, scenario):
-    # Fecha o navegador após cada cenário
-    if hasattr(context, "driver"):
-        context.driver.quit()
+    context.driver.quit()
